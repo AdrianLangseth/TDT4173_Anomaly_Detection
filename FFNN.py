3b3135faaa232a1c5329780387e28f82eb1b8311
Adrian <ad.langseth@gmail.com>
2020-10-27 19:26:20 +0100

    Swaps functional to FFNN from FFNN2; Deletes not functional original FFNN.

--- FFNN.py
+++ FFNN.py
@@ -1,11 +1,9 @@
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import tensorflow.keras.activations as act
+import tensorflow.keras.layers as KL
+import tensorflow.keras.models as KM
 
-import matplotlib.pyplot as plt
-
-
+# Dataset
 def load_data(path):
     with np.load(path) as f:
         x_train, y_train = f['x_train'], f['y_train']  # 60000 x 28 x 28, 600000
@@ -14,97 +12,19 @@ def load_data(path):
 
 
 (x_train, y_train), (x_test, y_test) = load_data('mnist.npz')
+x_train, x_test = x_train/255.0, x_test/255.0
 
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# Model
+inputs = KL.Input(shape=(28, 28))
+l = KL.Flatten()(inputs)
+l = KL.Dense(512, activation=act.relu)(l)
+outputs = KL.Dense(10, activation=act.softmax)(l)
 
+model = KM.Model(inputs, outputs)
+model.summary()
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=10)
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print("Test Loss: {0} - Test Acc: {1}".format(test_loss, test_acc))
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout2d(0.25)
-        self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    # x represents our data
-    def forward(self, x):
-        # Pass data through conv1
-        x = self.conv1(x)
-        # Use the rectified-linear activation function over x
-        x = F.relu(x)
-
-        x = self.conv2(x)
-        x = F.relu(x)
-
-        # Run max pooling over x
-        x = F.max_pool2d(x, 2)
-        # Pass data through dropout1
-        x = self.dropout1(x)
-        # Flatten x with start_dim=1
-        x = torch.flatten(x, 1)
-        # Pass data through fc1
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-
-        # Apply softmax to x
-        output = F.log_softmax(x, dim=1)
-        return output
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-
-# Equates to one random 28x28 image
-random_data = torch.rand((1, 1, 28, 28))
-
-net = Net()
-print(net)
-
-"""
-params = list(net.parameters())
-print(len(params))
-print(params[0].size())
-
-result = net(random_data)
-
-print(result)
-
-net.zero_grad()
-result.backward(torch.randn(1, 10))
-"""
-n
-
-
-output = net(random_data)
-target = torch.randn(10)  # a dummy target, for example
-target = target.view(1, -1)  # make it the same shape as output
-criterion = nn.MSELoss()
-
-loss = criterion(output, target)
-print(loss)
-
-print(loss.grad_fn)  # MSELoss
-print(loss.grad_fn.next_functions[0][0])  # Linear
-print(loss.grad_fn.next_functions[0][0].next_functions[0][0])  # ReLU
-
-net.zero_grad()     # zeroes the gradient buffers of all parameters
-
-print('conv1.bias.grad before backward')
-print(net.conv1.bias.grad)
-
-loss.backward()
-
-print('conv1.bias.grad after backward')
-print(net.conv1.bias.grad)
-
-learning_rate = 0.01
-for f in net.parameters():
-    f.data.sub_(f.grad.data * learning_rate)
+model.save("ffnn_model")
