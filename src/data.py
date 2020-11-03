47aae99bb1479f68953e29454d55124ecb4c2e5d
Jonatan Engstad <jonaengs@gmail.com>
2020-11-03 12:06:36 +0100

    remove old bnet implementation and leftover code. Restructure remaining code. Add logging of metadata and loading/saving of models. Begin notmnist integration

--- src/data.py
+++ src/data.py
@@ -6,19 +6,30 @@ import torchvision.transforms as transforms
 batch_size = 256  # going higher than 43 results in NaN results if using SGD
 
 # TODO: Do we want to normalize the data? We probably do. If so, agree on common parameters
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    # transforms.Normalize((0.5, ), (0.5, ))
-])
+normalize = False
+if normalize:
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, ), (0.5, ))
+    ])
+else:
+    transform = transforms.Compose([transforms.ToTensor()])
 
+training_set_size = 50_000
+test_set_size = 10_000
 val_set_size = 10_000
+
 # Training set is 60k ims, test set is 10k ims. 
 # TODO: Agree on where validation set will come from and its size. I propose 10k from train
-test_set = torchvision.datasets.MNIST(root='../data', train=False, download=True, transform=transform)
-train_set, val_set = torch.utils.data.random_split(
-    torchvision.datasets.MNIST(root='../data', train=True, download=True, transform=transform),
-    [60_000 - val_set_size, val_set_size]
+test_set, _ = torch.utils.data.random_split(
+    torchvision.datasets.MNIST(root='../data', train=False, download=True, transform=transform),
+    [test_set_size, 10_000 - test_set_size]
 )
+train_set, val_set, _ = torch.utils.data.random_split(
+    torchvision.datasets.MNIST(root='../data', train=True, download=True, transform=transform),
+    [training_set_size, val_set_size, 60_000 - (training_set_size + val_set_size)]
+)
+
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=True)
--- src/main.py
+++ src/main.py
@@ -1,20 +1,50 @@
+import numpy as np
 import torch
-import torchvision
-import torchvision.transforms as transforms
 import pyro
+import pprint
+import os
+import random
+from glob import glob
+from PIL import Image
 
-from data import train_loader, test_loader, val_loader
+import torch.nn.functional as F
+from pathlib import Path
 
-from BNet import BayesNet
+from data import train_loader, test_loader, val_loader, batch_size, training_set_size, normalize
+from Net import train, guide, model, num_epochs, predict_labels, \
+    get_prediction_confidence, metadata, accuracy_all, accuracy_exclude_uncertain, lr
 
-pyro.clear_param_store()
-net = BayesNet()
-net.infer_parameters(train_loader)
+normstr = "" if normalize else "_nonorm"
+fname = f"{training_set_size}t_{num_epochs}eps_{lr}lr{normstr}"
+model_path = f"../models/bnn/{fname}.model"
+metadata_path = f"../meta/bnn/{fname}.txt"
 
-total = 0.0
-correct = 0.0
-for images, labels in val_loader:
-    pred = net.forward(images.view(-1, 784), n_samples=1)
-    total += labels.size(0)
-    correct += (pred.argmax(-1) == labels).sum().item()
-print(f"Test accuracy: {correct / total * 100:.5f}")
+notmnist_path = "../data/notMNIST_small/"
+notmnist_files = [
+    os.path.join(notmnist_path, directory, fn)
+    for directory in os.listdir(notmnist_path)
+    for fn in os.listdir(os.path.join(notmnist_path, directory))
+]
+notmnist_iter = iter(random.sample(notmnist_files, len(notmnist_files)))
+def get_notmnist_batch(batch_size=32):
+    return np.array([
+        np.asarray(Image.open(next(notmnist_iter)))
+        for _ in range(batch_size)
+    ])
+
+
+if __name__ == '__main__':
+    assert False
+    if Path(model_path).is_file():
+        pyro.get_param_store().load(model_path)
+        print(accuracy_all())
+        pprint.pprint(accuracy_exclude_uncertain())
+    else:
+        train()
+        pprint.pprint(metadata)
+        pyro.get_param_store().save(model_path)
+        with open(metadata_path, mode="a+") as f:
+            f.write(pprint.pformat(metadata))
+
+    
+
